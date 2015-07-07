4f35d86ce5d005596246405be70f8a953544a570
alvin_li <alvin_li@htc.com>
2015-07-07 10:54:39 +0800

    Consummate bubble sort

---
+++ BubbleSortDemo/src/com/BubbleSort.java
@@ -0,0 +1,81 @@
+package com;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] arraya = new int[] { 5, 56, 45, 46, 14, 755, 15, 87, 71, 594, 1,
+                47, 564, 22, 41 };
+        int[] arrayb = new int[] { 5, 56, 45, 46, 14, 755, 15, 87, 71, 594, 1,
+                47, 564, 22, 41 };
+        int[] arrayc = new int[] { 5, 56, 45, 46, 14, 755, 15, 87, 71, 594, 1,
+                47, 564, 22, 41 };
+        int[] arrayd = new int[] { 5, 56, 45, 46, 14, 755, 15, 87, 71, 594, 1,
+                47, 564, 22, 41 };
+        bubbleSorta(arraya);
+        bubbleSortb(arrayb);
+        bubbleSortc(arrayc);
+        bubbleSortd(arrayd);
+    }
+
+    public static void mPrintln(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+        System.out.println("==========================");
+    }
+
+    public static void bubbleSorta(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+        mPrintln(a);
+    }
+
+    public static void bubbleSortb(int[] a) {
+        int temp = 0;
+        for (int i = a.length - 1; i > 0; --i) {
+            for (int j = 0; j < i; ++j) {
+                if (a[j + 1] < a[j]) {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+        mPrintln(a);
+    }
+
+    public static void bubbleSortc(int[] a) {
+        int temp = 0;
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = a.length - 1; j > i; j--) {
+                if (a[j] < a[j - 1]) {
+                    temp = a[j - 1];
+                    a[j - 1] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        mPrintln(a);
+    }
+
+    public static void bubbleSortd(int[] a) {
+        int temp = 0;
+        for (int i = a.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (a[j + 1] < a[j]) {
+                    temp = a[j + 1];
+                    a[j + 1] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        mPrintln(a);
+    }
+}
