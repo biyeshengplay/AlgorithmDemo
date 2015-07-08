55843c4bc15f75f18ec5c99e7ce23afa26bdf6d1
alvin_li <alvin_li@htc.com>
2015-07-08 16:10:23 +0800

    Add Double Bubble Sort

--- SortDemo/src/com/BubbleSort.java
+++ SortDemo/src/com/BubbleSort.java
@@ -16,6 +16,7 @@ public class BubbleSort {
         bubbleSortc(arrayc);
         bubbleSortd(arrayd);
         cocktailSort(arraye);
+        doubleBubbleSort(arraye);
     }
 
     public static void mPrintln(int[] a) {
@@ -100,5 +101,26 @@ public class BubbleSort {
             }
         }
         mPrintln(src);
+    }
+
+    public static void doubleBubbleSort(int[] a) {
+        for (int i = 0; i < a.length / 2; i++) {
+            for (int j = i; j < a.length - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+
+            for (int j = a.length - 1 - (i + 1); j > i; j--) {
+                if (a[j - 1] > a[j]) {
+                    int temp = a[j - 1];
+                    a[j - 1] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        mPrintln(a);
     }
 }
